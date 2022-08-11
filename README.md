ddb2082f1fcd8bbdb5dd92ef1afc16992b1930d8
sharp3614 <knurhak09@gmail.com>
2022-08-11 07:58:30 +0300

    win conditions added and readme updated

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Odev-2
 
 ## Grup Üyeleri
-- [Ad] [Soyad]
-- [Ad] [Soyad]
+- [Ahmet Salih Karagöz](https://github.com/krgzsalih)
+- [Vedat Eroğlu](https://github.com/vkeycode)
+- [Nurhak Keskin](https://github.com/sharp3614)
 
 ## İçerik
 
--  [Figma ve css örneği](https://www.figma.com/file/9WxsY4qgl7Elca9cv9Y5z6/Untitled?node-id=0%3A1)
+- [Figma ve css örneği](https://www.figma.com/file/9WxsY4qgl7Elca9cv9Y5z6/Untitled?node-id=0%3A1)
 - [Ödevin Örnek Videosu](https://www.loom.com/share/f6db31129ce64027b011a872dd91b867)
 
--- tictactoe/src/Components/Boxes.js
+++ tictactoe/src/Components/Boxes.js
@@ -1,26 +1,56 @@
 import { useEffect, useState } from "react";
 import "../App.css";
 
+const check = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8], 
+  [0,3,6], 
+  [1,4,7], 
+  [2,5,8], 
+  [0,4,8], 
+  [2,4,6]
+];
+
 function App() {
-  const [box, setBox] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [boxes, setBoxes] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [end, setEnd] = useState(false);
   const tictoctoe = (index) => {
-    if (box[index] === 0) {
-      box[index] = 1;
-    } else if (box[index] === 1) {
-      box[index] = 2;
-    } else {
-      box[index] = 0;
+    if (!end) {
+      if (boxes[index] === 0) {
+        boxes[index] = 1;
+      } else if (boxes[index] === 1) {
+        boxes[index] = 2;
+      } else {
+        boxes[index] = 0;
+      }
+      setBoxes([...boxes]);
+    }else {
+      setBoxes(Array(9).fill(0));
+      setEnd(false);
     }
-    setBox([...box]);
   };
+
+
+  useEffect(() => {
+    check.forEach((i) => {
+      if (boxes[i[0]] === 1 && boxes[i[1]] === 1 && boxes[i[2]] === 1) {
+        setEnd(true);
+      }
+      if (boxes[i[0]] === 2 && boxes[i[1]] === 2 && boxes[i[2]] === 2) {
+        setEnd(true);
+      }
+    });
+  }, [boxes]);
   return (
     <div className="App">
       <header className="App-header">
         <div className="text">GAME TIME</div>
         <div className="group">
-          {box.map((item, index) => {
+          {boxes.map((item, index) => {
             return (
               <div
+              key={"box_"+index}
                 className={
                   "rectangle " +
                   (item === 0
